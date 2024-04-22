551c4b73bea43de3eacabf45bdc465e9d315436c
Bonaiti.20104 <Bonaiti.20104@LABI1-13PC03>
2024-04-22 12:10:31 +0200

    Aggiungere i file di progetto.

---
+++ es thread/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+
+namespace es_thread
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Thread th = new Thread(WriteY);
+
+            th.Start();
+
+            Console.WriteLine("Timer iniziato");
+
+            System.Threading.Thread.Sleep(11000);
+
+            Console.WriteLine("SVEGLIAAAA !"); 
+
+            Console.ReadKey();
+
+        }
+
+        private static void WriteY()
+        {
+
+            for (int i = 10; i > 0; i--)
+            {
+
+                System.Threading.Thread.Sleep(1000);
+
+                Console.Write(i + " ");
+
+            }
+
+        }
+    }
+}  
